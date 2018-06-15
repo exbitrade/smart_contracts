860f1319d4bc758c0ac8b3a6708a8b518e81bcbb
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2018-06-15 19:23:47 +0530

    updated Phown.sol, updated readme

--- dev/Phown.sol
+++ dev/Phown.sol
@@ -1,9 +1,5 @@
 pragma solidity ^0.4.16;
 
-// 0xca35b7d915458ef540ade6068dfe2f44e8fa733c
-// 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c
-// 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db
-
 contract Phown {
     
     struct Data {
@@ -27,7 +23,7 @@ contract Phown {
         items[msg.sender][imei] = Data({imei: imei, marked: false, ownershipCount: 0});
         reverseItems[imei] = msg.sender;
         
-        Register(msg.sender, imei);
+        emit Register(msg.sender, imei);
     }
     
     function transfer(uint imei, address addr) public {
@@ -39,22 +35,23 @@ contract Phown {
         items[msg.sender][imei] = Data({imei: 0, marked: false, ownershipCount: 0});
         reverseItems[imei] = addr;
         
-        Transfer(imei);
+        emit Transfer(imei);
     }
 
     function mark(uint imei) public {
         require(items[msg.sender][imei].imei == imei);
         require(items[msg.sender][imei].marked == false);
         items[msg.sender][imei].marked = true;
-        Mark(imei);
+        
+        emit Mark(imei);
     }
 
     function unmark(uint imei) public {
         require(items[msg.sender][imei].imei == imei);
         require(items[msg.sender][imei].marked == true);
         items[msg.sender][imei].marked = false;
-        Unmark(imei);
+        
+        emit Unmark(imei);
     }
 
 }
-
--- dev/README.md
+++ dev/README.md
@@ -1,5 +1,5 @@
 Smart Contracts provided in this folder are deployed to Rinkeby network.
 
 Addresses: 
-Phown - 0xc47cb0903e6caf9465d8d71e6747331e578d2a89
+Phown - 0x9916d9a348dfc08677b5c21bff70943f2d2b1791
 PhownDelegated - 0x4fd191eeca5c5f3d1cb93774c2357eff29fd7e95
